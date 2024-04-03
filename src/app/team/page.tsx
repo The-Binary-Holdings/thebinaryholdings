fb7217565746e22442874834080b41cc35b61925
Elton Tay <57287843+eltontay@users.noreply.github.com>
2024-04-03 13:07:23 +0700

    Added Partners

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -50,49 +50,132 @@ const Team: NextPage = () => {
     {
       name: "Manish Jha ",
       role: "MD - Digital Social",
-      imageUrl: "/team/manish_jha.jpeg", 
+      imageUrl: "/team/manish_jha.jpeg",
       linkedinUrl: "https://www.linkedin.com/in/mjha86/",
     },
- 
+
     {
       name: "Sarah Intarapromma",
       role: "Chief Of Staff",
-      imageUrl: "/team/sarah.jpeg", 
+      imageUrl: "/team/sarah.jpeg",
       linkedinUrl: "https://www.linkedin.com/in/sarah-intarapromma-544b3ba/",
     },
-     
+
     {
       name: "Man Pham",
       role: "CEO - Move To Earn",
-      imageUrl: "/team/man_pham.jpeg", 
-      linkedinUrl: "https://www.linkedin.com/in/Business Development Director - SEA",
+      imageUrl: "/team/man_pham.jpeg",
+      linkedinUrl:
+        "https://www.linkedin.com/in/Business Development Director - SEA",
     },
 
     {
       name: "Daniel Grove",
       role: "CEO - Move To Earn",
-      imageUrl: "/team/daniel_grove.jpeg", 
+      imageUrl: "/team/daniel_grove.jpeg",
       linkedinUrl: "https://www.linkedin.com/in/daniel-grove-81b3501a3/",
     },
+  ];
 
+  const partnerMembers = [
+    {
+      name: "Mario Nawfal",
+      role: "Founder & CEO , IBC",
+      imageUrl: "/team/mario_nawfal.jpeg",
+      linkedinUrl: "https://www.linkedin.com/in/mario-nawfal/",
+    },
+
+    {
+      name: "Dave Shin",
+      role: "BD @ Immutable",
+      imageUrl: "/team/dave_shin.jpg", // Replace with actual path to image
+      linkedinUrl: "https://www.linkedin.com/in/shin-novation/",
+    },
+    {
+      name: "David Ng",
+      role: "Serial Entrepreneur & Investor",
+      imageUrl: "/team/david_ng.jpeg", // Replace with actual path to image
+      linkedinUrl: "https://www.linkedin.com/in/davidngmw/",
+    },
+    {
+      name: "Henry Ondo",
+      role: "COO/CFO/BD Coach",
+      imageUrl: "/team/male_placeholder.png", // Replace with actual path to image
+      linkedinUrl: "https://www.linkedin.com/in/henry-ondo-b18a5765/",
+    },
+    {
+      name: "Daniel Yang",
+      role: "Head of Treasury - Algorand Foundation",
+      imageUrl: "/team/daniel_yang.jpeg", // Replace with actual path to image
+      linkedinUrl: "https://www.linkedin.com/in/daniel-yang-b7989217/",
+    },
+    {
+      name: "Kai Kono",
+      role: "CEO at Zodia Custody",
+      imageUrl: "/team/kai_kono.jpeg", // Replace with actual path to image
+      linkedinUrl: "https://www.linkedin.com/in/kai-kono-86542b1b",
+    },
+    {
+      name: "Nathnael Seat",
+      role: "Investor at Ninepine Technologies",
+      imageUrl: "/team/nathanael_seet.jpeg", // Replace with actual path to image
+      linkedinUrl: "https://www.linkedin.com/in/nathanael-seet-9726371aa",
+    },
+    {
+      name: "Sky Wee",
+      role: "Crypto & Gaming Influencer",
+      imageUrl: "/team/sky_wee.jpeg", // Replace with actual path to image
+      linkedinUrl: "https://www.linkedin.com/in/skywee97",
+    },
+    {
+      name: "Nikita Sachdev",
+      role: "CEO, Luna Media",
+      imageUrl: "/team/nikita_sachdev.jpeg",
+      linkedinUrl: "https://www.linkedin.com/in/nikitasachdev/",
+    },
+
+    {
+      name: "Gilly Strauss",
+      role: "Chairman, Strauss Capital",
+      imageUrl: "/team/gilly_strauss.jpeg",
+      linkedinUrl: "https://www.linkedin.com/in/gilly-strauss-6ba149/",
+    },
+
+    {
+      name: "Joanne Wood",
+      role: "Founder & Chairman, Capital Eight",
+      imageUrl: "/team/joanne_wood.jpeg",
+      linkedinUrl:
+        "https://www.linkedin.com/in/joanne-wood-a4a2486/",
+    },
   ];
 
   return (
-    <div className="pt-16 md:px-8">
-      <Head>
-        <title>Our Team</title>
-      </Head>
-
-      <main className="bg-black text-white p-10">
-        <section>
-          <h2 className="text-4xl font-bold mb-10">Our Team</h2>
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-            {teamMembers.map((member, index) => (
-              <TeamMember key={index} {...member} />
-            ))}
-          </div>
-        </section>
-      </main>
+    <div>
+      <div className="pt-16 md:px-8">
+        <main className="bg-black text-white p-10">
+          <section>
+            <h2 className="text-4xl font-bold mb-10">Our Team</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+              {teamMembers.map((member, index) => (
+                <TeamMember key={index} {...member} />
+              ))}
+            </div>
+          </section>
+        </main>
+      </div>
+      <div className="pt-16 md:px-8">
+        <main className="bg-black text-white p-10">
+          <section>
+            <h2 className="text-4xl font-bold mb-10">Our Partners</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+              {partnerMembers.map((member, index) => (
+                <TeamMember key={index} {...member} />
+              ))}
+            </div>
+          </section>
+        </main>
+      </div>
     </div>
   );
 };
