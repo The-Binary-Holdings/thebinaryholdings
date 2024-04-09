cd3e900535c1145dfad845e199ee287af88ab325
Elton Tay <57287843+eltontay@users.noreply.github.com>
2024-04-09 17:52:29 +0700

    Push analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Layout from "@/components/layouts/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-  title: "The Binary Holdings (TBH) - The Web3 Infrastructure Provider For Telcos",
-  description: "The Binary Holdings empowers developing and emerging economies by providing a web3 infrastructure that offers scalable, low-cost, and efficient web3 infrastructure services",
+  title:
+    "The Binary Holdings (TBH) - The Web3 Infrastructure Provider For Telcos",
+  description:
+    "The Binary Holdings empowers developing and emerging economies by providing a web3 infrastructure that offers scalable, low-cost, and efficient web3 infrastructure services",
 };
 
 export default function RootLayout({
@@ -12,5 +15,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <Layout>{children}</Layout>;
+  return (
+    <Layout>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </Layout>
+  );
 }
