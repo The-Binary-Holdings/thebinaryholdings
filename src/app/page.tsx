e3ff554a6d5269d0862020bb8f27402153575b58
tvh050423@gmail.com <tvh050423@gmail.com>
2024-04-11 23:02:40 +0700

    fix: comment out service and communiation section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <VisionSection />
       <BusinessSection />
       <PartnerSection />
-      <ServiceSection />
-      <CommunicationSection />
+      {/* <ServiceSection />
+      <CommunicationSection /> */}
     </main>
   );
 }
